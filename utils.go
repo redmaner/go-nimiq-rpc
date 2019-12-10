4127490a6b91a60103b5bfec3fa8f0a57819a551
Jake van der Putten <jakevdputten@gmail.com>
2019-12-10 16:01:03 +0100

    Remove unnecessary function

--- utils.go
+++
@@ -1,8 +0,0 @@
-package nimiqrpc
-
-import "encoding/hex"
-
-// AddressToHex converts a Nimiq address to a 20 byte hex encoded address
-func AddressToHex(address string) string {
-	return hex.EncodeToString([]byte(address))
-}
